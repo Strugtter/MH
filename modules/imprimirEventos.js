ae580a37b85ace928362a7be7cce6da55f7d1fc6
Camilo Botina Trujillo <camilobotinatrujillo@gmail.com>
2023-03-09 18:12:05 -0500

    Modifique el filtro de las fechas

---
+++ modules/imprimirEventos.js
@@ -0,0 +1,23 @@
+export const printEvents = (container, arrayEventos) => {
+    container.innerHTML = "";
+
+    arrayEventos.forEach((eventos) => {
+        const div = document.createElement("div");
+        div.classList.add("col-3");
+        div.innerHTML = `
+            <div class="border px-2">
+            <figure class="border mt-2">
+              <img src="../image/Maraton3.jpg" class="img-fluid" alt="">
+            </figure>
+            <h2 class="text-center">Maratón</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.</p>
+            <div class="d-grid d-md-flex mb-2 justify-content-md-between">
+              <h5>Price $4000</h5>
+              <a class="btn btn-primary" href="../pages/details.html" role="button">detalles</a>
+            </div>
+          </div>
+        `;
+        container.appendChild(div);
+    });
+};
--- script/main.js
+++ script/main.js
@@ -1,22 +1,25 @@
  import {data} from "../data/date.js";
+ 
 
 let fechasPasadas = [];
 let fechasFuturas = [];
 let fechasActual = Date.parse(data.fechaActual);
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 // Seleccion de fechas pasadas y futuras respecto a la fecha actula "2022-01-01". 
 
 for (let i = 0; i < data.eventos.length;i++){
     let fechaEvento = Date.parse(data.eventos[i].date);
-    let fechaAgregada = new Date ();
-    fechaAgregada.setTime(fechaEvento);
     if(fechaEvento > fechasActual){
-        fechasFuturas.push(fechaAgregada.toLocaleDateString('en-US', options));
+        fechasFuturas.push(data.eventos[i]);
     } else {
-        fechasPasadas.push(fechaAgregada.toLocaleDateString('en-US', options));
+        fechasPasadas.push(data.eventos[i]);
     }
 }
+
+console.log(fechasFuturas);
+// 
+
+//const containerProperties = document.getElementById("ContainerEvents");
 
 
 
